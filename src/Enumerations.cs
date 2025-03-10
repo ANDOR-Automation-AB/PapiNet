bf9cf912f5aa18dec5ff6709eb9b6b25c21e16a3
Johan Krusing <johan.krusing@hotmail.com>
2025-03-10 07:05:17 +0100

    Add MoistureContentPercentage

--- src/Enumerations.cs
+++ src/Enumerations.cs
@@ -3312,3 +3312,30 @@ public enum SeasoningType
     SurfacedDry,
     SurfacedGreen,
 }
+
+public enum MoistureContentPercentage
+{
+    _1,
+    _2,
+    _3,
+    _4,
+    _5,
+    _6,
+    _7,
+    _8,
+    _9,
+    _10,
+    _11,
+    _12,
+    _13,
+    _14,
+    _15,
+    _16,
+    _18,
+    _19,
+    _20,
+    _21,
+    _22,
+    _23,
+    Other,
+}
